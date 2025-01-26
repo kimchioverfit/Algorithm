12586d6196a02e12554e09ff670e3c2e7d7fda9f
김영준 <god102104@lginnotek.com>
2025-01-26 23:13:11 +0900

    [Error] Out of intelligence

--- test.cc
+++ test.cc
@@ -1,48 +1,38 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <numeric>
 
 using namespace std;
 
-class Solution {
-public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> results;
-        vector<int> current;
-        vector<bool> used(nums.size(), false);
-        backtrack(nums, results, current, used);
-        return results;
+int findTargetSumWays(vector<int>& nums, int target) {
+    int totalSum = accumulate(nums.begin(), nums.end(), 0);
+    
+    // If the target sum is not achievable or invalid
+    if ((totalSum - target) < 0 || (totalSum - target) % 2 != 0) {
+        return 0;
     }
-
-private:
-    void backtrack(vector<int>& nums, vector<vector<int>>& results, vector<int>& current, vector<bool>& used) {
-        if (current.size() == nums.size()) {
-            results.push_back(current);
-            return;
+    
+    int subsetSum = (totalSum - target) / 2;
+    
+    // DP vector to store the number of ways to achieve each sum
+    vector<int> dp(subsetSum + 1, 0);
+    dp[0] = 1; // Base case: one way to achieve sum 0 (empty subset)
+    
+    // Update DP array
+    for (int num : nums) {
+        for (int j = subsetSum; j >= num; --j) {
+            dp[j] += dp[j - num];
         }
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            if (used[i]) continue;
-            used[i] = true;
-            current.push_back(nums[i]);
-            backtrack(nums, results, current, used);
-            current.pop_back();
-            used[i] = false;
-        }
+        cout<< "d"<<endl;
     }
-};
+    
+    return dp[subsetSum];
+}
 
 int main() {
-    Solution sol;
-    vector<int> nums = {1, 2, 3};
-    vector<vector<int>> permutations = sol.permute(nums);
+    vector<int> nums = {1, 1, 2, 2, 2,3,3,6};
+    int target = 6;
     
-    for (const auto& perm : permutations) {
-        for (int num : perm) {
-            cout << num << " ";
-        }
-        cout << endl;
-    }
-    
+    cout << "Number of ways to achieve target: " << findTargetSumWays(nums, target) << endl;
     return 0;
 }
